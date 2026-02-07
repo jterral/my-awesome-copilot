1de72941e8013ccf7ad616a040706a7523418145
Jonathan <jonathan.terral@outlook.com>
2026-02-07 19:34:27 +0100

    feat(pullrequest): use mise-shared-taks, and add pull request instructions (#2)

--- .mise-tasks/precommit/run-all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#MISE description="Run all pre-commit tasks"
-#MISE depends=["precommit:check-prerequisites"]
-
-set -e
-
-echo "🔎 Running all pre-commit tasks..."
-
-pre-commit run --all-files
--- .mise-tasks/precommit/update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#MISE description="Update pre-commit hooks"
-#MISE depends=["precommit:check-prerequisites"]
-
-set -e
-
-echo "🔄 Updating pre-commit hooks..."
-
-pre-commit autoupdate
